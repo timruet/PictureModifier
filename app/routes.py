846d63a900f85f6bacf8ca108a3aac29e19271d7
timruet <tr.timruettinger@gmail.com>
2023-07-13 17:20:44 +0200

    picture no longer gets saved

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, flash, redirect, url_for, request, current_app, send_from_directory
+from flask import render_template, flash, redirect, url_for, request, current_app, send_file
 from app import app
 import os
+import io
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
 from .util import validate_image
@@ -15,16 +16,26 @@ def index():
         image_size_y = request.form['image_size_y']
         image_size_x = request.form['image_size_x']
         filename = secure_filename(uploaded_file.filename)
+        
         if filename != '':
             file_ext = os.path.splitext(filename)[1]
             if file_ext not in current_app.config['UPLOAD_EXTENSIONS'] or file_ext != validate_image(uploaded_file.stream): 
                 abort(400)
+
             uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
             img = Image.open(f'uploads/{filename}') # Open image
             img = img.resize((int(image_size_y), int(image_size_x)), Image.LANCZOS) # Resize image
             img.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-            directory=os.path.join(basedir, app.config['UPLOAD_PATH'])
-            return send_from_directory(directory=directory, path=filename, as_attachment=True)
+            file_path=os.path.join(basedir, app.config['UPLOAD_PATH'],filename)
+
+            return_data = io.BytesIO()
+            with open(file_path, 'rb') as fo:
+                return_data.write(fo.read())
+                # (after writing, cursor will be at last byte, so move it to start)
+            return_data.seek(0)
+            os.remove(file_path)
+            return send_file(return_data,mimetype=f'image/{file_ext.replace(".","")}', as_attachment=True, download_name=filename)
+        
         return redirect(url_for('index'))
     return render_template('index.html')
 
